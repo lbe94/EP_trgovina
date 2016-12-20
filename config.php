f90c7c878f906253ac11bbbcb9da2d79834c368a
Luka <luka.bezovsek@gmail.com>
2016-12-20 16:51:29 +0100

    test komentar

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+    // database configuration
     define('DB_SERVER', 'localhost:3306');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
--- index.php
+++ index.php
@@ -22,9 +22,11 @@ include('index_session.php')
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $name ?>
                     <span class="glyphicon glyphicon-user"></span></a>
                 <ul class="dropdown-menu col-md-10">
-                        <a href="#" class="btn btn-default btn-lg col-lg-10 col-lg-offset-1" style="margin-top: 1%">Moji nakupi</a>
-                        <a href="#" class="btn btn-default btn-lg col-lg-10 col-lg-offset-1" style="margin-top: 1%">Moj profil</a>
-                        <a href="logout.php" class="btn btn-danger btn-lg col-md-10 col-lg-offset-1" style="margin-top: 1%">Odjava</a>
+                    <a href="#" class="btn btn-default btn-lg col-lg-10 col-lg-offset-1" style="margin-top: 1%">Moji
+                        nakupi</a>
+                    <a href="#" class="btn btn-default btn-lg col-lg-10 col-lg-offset-1" style="margin-top: 1%">Moj
+                        profil</a>
+                    <a href="logout.php" class="btn btn-danger btn-lg col-md-10 col-lg-offset-1" style="margin-top: 1%">Odjava</a>
                 </ul>
             </li>
         </ul>
@@ -32,21 +34,23 @@ include('index_session.php')
 </nav>
 <div class="container">
     <?php
-        while($result = mysqli_fetch_array($select_articles, MYSQLI_ASSOC)){?>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h2 class="h2"><?php echo $result['Naziv']?></h2>
-                </div>
-                <div class="panel-body">
-                    <p1 class="h5"><?php echo $result['Opis']?></p1>
-                </div>
-                <div class="panel-footer">
-                    <p1 class="h3 text-danger pull-right"><?php echo $result['Cena']." "?><span class="glyphicon-euro"></span></p1>
-                    <a href="#" class="btn btn-success btn-lg">V košarico</a>
-                </div>
+    while ($result = mysqli_fetch_array($select_articles, MYSQLI_ASSOC)) {
+        ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="h2"><?php echo $result['Naziv'] ?></h2>
             </div>
-    <?php
-        }
+            <div class="panel-body">
+                <p1 class="h5"><?php echo $result['Opis'] ?></p1>
+            </div>
+            <div class="panel-footer">
+                <p1 class="h3 text-danger pull-right"><?php echo $result['Cena'] . " " ?><span
+                        class="glyphicon-euro"></span></p1>
+                <a href="#" class="btn btn-success btn-lg">V košarico</a>
+            </div>
+        </div>
+        <?php
+    }
     ?>
 </div>
 </body>
