f5af0266baaa10cf848b613722bb9032efb3ee00
Luka <luka.bezovsek@gmail.com>
2017-01-21 15:49:13 +0100

    Dodani demo skripti za vračanje artiklov in strank (za potrebe testiranja android network operacij)

---
+++ demo_artikli.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luka
+ * Date: 21. 01. 2017
+ * Time: 14:42
+ */
+
+$artikli = '[
+  {
+    "ID": "1",
+    "Naziv": "Apple iPad mini 3 Wi-Fi 16GB Space Gray",
+    "Opis": "Najnaprednej\u0161i iPad mini ima senzor prstnih odtisov Touch ID, 7,9-pal\u010dni zaslon Retina, zmogljiv \u010dip A7 s 64-bitno arhitekturo, kamero iSight, kamero FaceTime HD, hitro brez\u017ei\u010dno povezavo, iOS 8, iCloud in do 10 ur delovanja baterije.1 Prav tako je opremljen s \u0161tevilnimi odli\u010dnimi aplikacijami za delo in ustvarjalnost. \u0160tevilne druge aplikacije pa so na voljo v trgovini App Store. ",
+    "Zaloga": "20",
+    "Cena": "357",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "2",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik LENOVO Miix 3 W8.1 BING",
+    "Opis": "10,1-pal\u010dni tabli\u010dni ra\u010dunalnik Lenovo MIIX 3, ki deluje s sistemom Windows 8.1 Bing in \u0161tirijedrno procesorsko tehnologijo, je odli\u010den za produktivnost in zabavo na poti. Med lastnostmi, ki ga odlikujejo, so med drugim enoletna licenca za Office 365, sedemurna \u017eivljenjska doba baterije in cena, ki vam bo prav gotovo v\u0161e\u010d.",
+    "Zaloga": "0",
+    "Cena": "289.98",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "3",
+    "Naziv": "Apple iPad mini 3 Wi-Fi 16GB Gold",
+    "Opis": "Najnaprednej\u0161i iPad mini ima senzor prstnih odtisov Touch ID, 7,9-pal\u010dni zaslon Retina, zmogljiv \u010dip A7 s 64-bitno arhitekturo, kamero iSight, kamero FaceTime HD, hitro brez\u017ei\u010dno povezavo, iOS 8, iCloud in do 10 ur delovanja baterije.1 Prav tako je opremljen s \u0161tevilnimi odli\u010dnimi aplikacijami za delo in ustvarjalnost. \u0160tevilne druge aplikacije pa so na voljo v trgovini App Store.",
+    "Zaloga": "0",
+    "Cena": "357",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "4",
+    "Naziv": "Apple iPad mini 3 Wi-Fi 16GB Silver",
+    "Opis": "Najnaprednej\u0161i iPad mini ima senzor prstnih odtisov Touch ID, 7,9-pal\u010dni zaslon Retina, zmogljiv \u010dip A7 s 64-bitno arhitekturo, kamero iSight, kamero FaceTime HD, hitro brez\u017ei\u010dno povezavo, iOS 8, iCloud in do 10 ur delovanja baterije.1 Prav tako je opremljen s \u0161tevilnimi odli\u010dnimi aplikacijami za delo in ustvarjalnost. \u0160tevilne druge aplikacije pa so na voljo v trgovini App Store.",
+    "Zaloga": "0",
+    "Cena": "357",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "5",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik LENOVO YOGA TAB 3 10\" ",
+    "Opis": "LENOVO YOGA TAB 3 10\" - tabli\u010dni ra\u010dunalnik izdelan za va\u0161e udobje!",
+    "Zaloga": "0",
+    "Cena": "209.99",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "6",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik XPLORE Atheros XP8772",
+    "Opis": "Tabli\u010dni ra\u010dunalnik XPLORE XP8772 vam nudi obilo zabave pri brskanju po internetu, poslu\u0161anju glasbe ali igranju osnovnih iger.",
+    "Zaloga": "0",
+    "Cena": "54.99",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "7",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik Trekstor SurfTab Wintron ",
+    "Opis": "Tabli\u010dni ra\u010dunalnik, pri katerem naj vas ne zavede nizka cena. Poganja ga Intelov procesor, ima 1 GB delovnega pomnilnika in nalo\u017een operacijski sistem Windows 10. Konfiguracija, ki bo z lahkoto kos enostavnej\u0161im opravilom.",
+    "Zaloga": "0",
+    "Cena": "79.98",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "8",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik LENOVO Miix 300 Z3735F 10",
+    "Opis": "10-pal\u010dni tabli\u010dni ra\u010dunalnik Lenovo MIIX 3, ki deluje s sistemom Windows 10 in \u0161tirijedrno procesorsko tehnologijo, je odli\u010den za produktivnost in zabavo na poti. ",
+    "Zaloga": "0",
+    "Cena": "259.99",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "9",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik LENOVO Tab A10-70",
+    "Opis": "Edinstvena tablica Lenovo A10\u201370 vas bo razvedrila s svojim velikim zaslonom in sposobnostmi. Tablica vam ponuja \u010dist in barvit pogled na dogajanje v digitalnem svetu. Zaslon je kot nala\u0161\u010d za gledanje filmov, slik in brskanje po spletu. Mogo\u010dni \u0161tiri-jedrni procesor vam omogo\u010di uporabo ve\u010dih programov isto\u010dasno. Izkusite teko\u010do in gladko Android izku\u0161njo. Ni\u010d ve\u010d ne boste imeli te\u017eav z zatikanjem pri igranju iger ali predvajanju HD filmov. Vse to in \u0161e ve\u010d po izjemni in ugodni ceni!",
+    "Zaloga": "0",
+    "Cena": "199.99",
+    "Aktiven": "0"
+  },
+  {
+    "ID": "10",
+    "Naziv": "Tabli\u010dni ra\u010dunalnik ASUS ZenPad 80",
+    "Opis": "Odli\u010den tabli\u010dni ra\u010dunalnik - majhen, lep, zmogliv - to je ASUS ZenPad 80, katerega lahko vzamete s seboj na pot, ga uporabljate na dopustu, v delovne namene in podobno. Tabli\u010dni ra\u010dunalnik vam bo na voljo za razli\u010dna opravila in vas ne bo nikoli razo\u010daral.",
+    "Zaloga": "0",
+    "Cena": "149.99",
+    "Aktiven": "0"
+  }
+]';
+
+echo $artikli;
+return $artikli;
---
+++ demo_stranke.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luka
+ * Date: 21. 01. 2017
+ * Time: 15:21
+ */
+
+$stranke = '[
+  {
+    "idStranke": "1",
+    "Ime": "Luka",
+    "Priimek": "Bezov\u0161ek",
+    "Eposta": "luka.bezovsek@gmail.com",
+    "Geslo": "7d1303e9680ea17c41a2cf6e79cd48ec",
+    "Aktiven": "0"
+  }
+]';
+
+echo $stranke;
+return $stranke;
